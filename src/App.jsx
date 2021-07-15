05c938c50c0b01bc649fc79d1a414b88f4cf0950
Adriana Mucciolo <drishaolin@gmail.com>
2021-07-15 15:39:38 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './components/Header'
+import Main from './components/Main'
+export default function App() {
+  console.log('Teste no console do navegador');
+
+  return (
+    <div>
+      <Header>React Elections</Header>
+
+      <Main></Main>
+    </div>
+  );
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,3 @@
+export default function Main({ children = 'Conteúdo Principal'}) {
+  return <main className="container mx-auto p-4">{children}</main>;
+}
