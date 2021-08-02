e08b1368f84a56d0eefebe9766203bd8a654980b
Adriana Mucciolo <drishaolin@gmail.com>
2021-08-02 19:28:26 -0300

    Create Elections.jsx component

---
+++ src/components/Elections.jsx
@@ -0,0 +1,31 @@
+export default function Elections({
+    children: candidates,
+    cityElectionData = {
+        name: "Município",
+        votingPopulation: 0,
+        absence: 0,
+        presence: 0,
+    },    
+}) {
+    let { name, votingPopulation, absence, presence } = cityElectionData;
+    const formattedNumber = (value) => new Intl.NumberFormat("pt-br").format(value);
+    return (
+        <div className="border-2 border-yellow-300 flex flex-col items-center">
+            <h2 className="font-bold">Eleição em {name}</h2>
+            <div className="space-x-10">
+                <span>
+                    <span className="font-medium">Total de eleitores: </span>
+                    {formattedNumber(votingPopulation)}
+                </span>
+                <span>
+                    <span className="font-medium">Abstenção: </span>
+                    {formattedNumber(absence)}
+                </span>
+                <span>
+                    <span className="font-medium">Comparecimento: </span>
+                    {formattedNumber(presence)}
+                </span>
+            </div>
+        </div>
+    );
+}
--- src/pages/ElectionsPage.jsx
+++ src/pages/ElectionsPage.jsx
@@ -1,42 +1,44 @@
 import { useState, useEffect } from "react";
 import { apiGetCities } from "../api/apiElections";
+import Elections from "../components/Elections";
 import Header from "../components/Header";
 import Main from "../components/Main";
 import Select from "../components/Select";
 
 export default function ElectionsPage() {
-  const [cities, setCities] = useState([]);
-  const [selectedCity, setSelectedCity] = useState("");
+    const [cities, setCities] = useState([]);
+    const [selectedCity, setSelectedCity] = useState("");
 
-  useEffect(() => {
-    async function getAllCities() {
-      const allCities = await apiGetCities();
-      setCities(allCities);
+    useEffect(() => {
+        async function getAllCities() {
+            const allCities = await apiGetCities();
+            setCities(allCities);
+        }
+        getAllCities();
+    }, []);
+
+    function handleCityChange(e) {
+        setSelectedCity(e.currentTarget.value);
     }
-    getAllCities();
-  }, []);
-  console.log("Effect: ", cities);
 
-  function handleCityChange(e) {
-    setSelectedCity(e.currentTarget.value);    
-  }
-  console.log("Selected City: ", selectedCity);
+    return (
+        <div>
+            <Header>React Elections</Header>
 
-  return (
-    <div>
-      <Header>React Elections</Header>
-
-      <Main>
-        <div className="flex flex-col items-center">
-          <Select
-            selectedValue = {selectedCity}
-            labelDescription="Selecione o Município"
-            onSelectChange={handleCityChange}
-          >
-            {cities.map(({id, name}) => ({ id: id, description: name }))}
-          </Select>
+            <Main>
+                <div className="flex flex-col items-center mb-4">
+                    <Select
+                        selectedValue={selectedCity}
+                        labelDescription="Selecione o Município"
+                        onSelectChange={handleCityChange}
+                    >
+                        {cities.map(({ id, name }) => ({ id: id, description: name }))}
+                    </Select>
+                </div>
+                <Elections
+                    cityElectionData={cities.find((city) => city.id === selectedCity)}
+                ></Elections>
+            </Main>
         </div>
-      </Main>
-    </div>
-  );
+    );
 }
